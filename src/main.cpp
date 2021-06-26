25a863fca9c2999ee00cf36de104d75e7144fdcd
SiTek <simon@siki>
2021-06-26 23:19:49 +0200

    Added appInfo and InstanceCreateInfo

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include "vulkan/vulkan.h"
 
-int main(int, char**) 
+int main(int argc, char *argv[]) 
 {
-    std::cout << "Hello, world!\n";
-    std::cout << "Test if everything worked!\n";
+    VkApplicationInfo applicationInfo;
+    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    applicationInfo.pNext = nullptr;
+    applicationInfo.pApplicationName = "VulkanPrototype";
+    applicationInfo.applicationVersion = VK_MAKE_VERSION(0,1,0);
+    applicationInfo.pEngineName = nullptr;
+    applicationInfo.engineVersion = 0;
+    applicationInfo.apiVersion = VK_API_VERSION_1_2;
+
+    VkInstanceCreateInfo instanceCreateInfo;
+    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceCreateInfo.pNext = nullptr;
+    instanceCreateInfo.flags = 0;
+    instanceCreateInfo.pApplicationInfo = &applicationInfo;
+    instanceCreateInfo.enabledLayerCount = 0;
+    instanceCreateInfo.ppEnabledLayerNames = nullptr;
+    instanceCreateInfo.enabledExtensionCount = 0;
+    instanceCreateInfo.ppEnabledExtensionNames = nullptr;
 
     return 0;
 }
