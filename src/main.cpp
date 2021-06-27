dc67549068461e4e31897a08325dfd0e82b7d3d6
SiTek <simon@siki>
2021-06-27 15:20:49 +0200

    Added enumeration over Physical devices

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
 #include "vulkan/vulkan.h"
 
-int main(int argc, char *argv[]) 
+#define VULKANPROTOTYPE_VERSION VK_MAKE_VERSION(0,1,0)
+
+namespace VulkanPrototype
 {
-    VkApplicationInfo applicationInfo;
-    applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-    applicationInfo.pNext = nullptr;
-    applicationInfo.pApplicationName = "VulkanPrototype";
-    applicationInfo.applicationVersion = VK_MAKE_VERSION(0,1,0);
-    applicationInfo.pEngineName = nullptr;
-    applicationInfo.engineVersion = 0;
-    applicationInfo.apiVersion = VK_API_VERSION_1_2;
+    
+    VkInstance instance;
 
-    VkInstanceCreateInfo instanceCreateInfo;
-    instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-    instanceCreateInfo.pNext = nullptr;
-    instanceCreateInfo.flags = 0;
-    instanceCreateInfo.pApplicationInfo = &applicationInfo;
-    instanceCreateInfo.enabledLayerCount = 0;
-    instanceCreateInfo.ppEnabledLayerNames = nullptr;
-    instanceCreateInfo.enabledExtensionCount = 0;
-    instanceCreateInfo.ppEnabledExtensionNames = nullptr;
+    void EvaluteVulkanResult(VkResult result)
+    {
+        if(result != VK_SUCCESS)
+            __debugbreak();
+    }
 
-    return 0;
+    int main(int argc, char *argv[]) 
+    {
+        VkApplicationInfo applicationInfo;
+        applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        applicationInfo.pNext = nullptr;
+        applicationInfo.pApplicationName = "VulkanPrototype";
+        applicationInfo.applicationVersion = VULKANPROTOTYPE_VERSION;
+        applicationInfo.pEngineName = nullptr;
+        applicationInfo.engineVersion = 0;
+        applicationInfo.apiVersion = VK_API_VERSION_1_2;
+
+        VkInstanceCreateInfo instanceCreateInfo;
+        instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        instanceCreateInfo.pNext = nullptr;
+        instanceCreateInfo.flags = 0;
+        instanceCreateInfo.pApplicationInfo = &applicationInfo;
+        instanceCreateInfo.enabledLayerCount = 0;
+        instanceCreateInfo.ppEnabledLayerNames = nullptr;
+        instanceCreateInfo.enabledExtensionCount = 0;
+        instanceCreateInfo.ppEnabledExtensionNames = nullptr;
+
+        VkResult result = vkCreateInstance(&instanceCreateInfo, nullptr ,&instance);
+        EvaluteVulkanResult(result);
+
+        uint32_t amountOfPhysicalDevices = 0;
+        result = vkEnumeratePhysicalDevices(instance, &amountOfPhysicalDevices, nullptr);
+        EvaluteVulkanResult(result);
+
+        VkPhysicalDevice* physicalDevices = new VkPhysicalDevice[amountOfPhysicalDevices];
+        result = vkEnumeratePhysicalDevices(instance, &amountOfPhysicalDevices, physicalDevices);
+        EvaluteVulkanResult(result);
+
+        std::cout << amountOfPhysicalDevices << std::endl;
+
+        return 0;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    VulkanPrototype::main(argc, argv);
 }
