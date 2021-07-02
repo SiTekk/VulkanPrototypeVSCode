d6a5564d101d5e3fa113290cf9d6c632575c2865
SiTek <simon@siki>
2021-07-02 22:33:22 +0200

    Activated Layers, Fixed Queue bug

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "vulkan/vulkan.h"
 
 #define VULKANPROTOTYPE_VERSION VK_MAKE_VERSION(0, 1, 0)
@@ -20,8 +21,6 @@ namespace VulkanPrototype
         }
     }
 
-    //Test Commit 4
-
     int main(int argc, char *argv[])
     {
         VkApplicationInfo applicationInfo;
@@ -34,30 +33,42 @@ namespace VulkanPrototype
         applicationInfo.apiVersion = VK_API_VERSION_1_2;
 
         uint32_t amountOfLayers = 0;
-        vkEnumerateInstanceLayerProperties(&amountOfLayers, nullptr);
+        VkResult result = vkEnumerateInstanceLayerProperties(&amountOfLayers, nullptr);
+        EvaluteVulkanResult(result);
         VkLayerProperties *layers = new VkLayerProperties[amountOfLayers];
-        vkEnumerateInstanceLayerProperties(&amountOfLayers, layers);
+        result = vkEnumerateInstanceLayerProperties(&amountOfLayers, layers);
+        EvaluteVulkanResult(result);
+
+        const std::vector<const char*> validationLayers = {
+            "VK_LAYER_KHRONOS_validation"
+        };
 
         VkInstanceCreateInfo instanceCreateInfo;
         instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         instanceCreateInfo.pNext = nullptr;
         instanceCreateInfo.flags = 0;
         instanceCreateInfo.pApplicationInfo = &applicationInfo;
-        instanceCreateInfo.enabledLayerCount = 0;
-        instanceCreateInfo.ppEnabledLayerNames = nullptr;
+        instanceCreateInfo.enabledLayerCount = validationLayers.size();
+        instanceCreateInfo.ppEnabledLayerNames = validationLayers.data();
         instanceCreateInfo.enabledExtensionCount = 0;
         instanceCreateInfo.ppEnabledExtensionNames = nullptr;
 
-        VkResult result = vkCreateInstance(&instanceCreateInfo, nullptr, &instance);
+        result = vkCreateInstance(&instanceCreateInfo, nullptr, &instance);
         EvaluteVulkanResult(result);
 
         uint32_t amountOfPhysicalDevices = 0;
         result = vkEnumeratePhysicalDevices(instance, &amountOfPhysicalDevices, nullptr);
         EvaluteVulkanResult(result);
-
         VkPhysicalDevice *physicalDevices = new VkPhysicalDevice[amountOfPhysicalDevices];
         result = vkEnumeratePhysicalDevices(instance, &amountOfPhysicalDevices, physicalDevices);
         EvaluteVulkanResult(result);
+
+        uint32_t amountOfQueueFamilyProperties = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(physicalDevices[0], &amountOfQueueFamilyProperties, nullptr);
+        VkQueueFamilyProperties* queueFamilyProperties = new VkQueueFamilyProperties[amountOfQueueFamilyProperties];
+        vkGetPhysicalDeviceQueueFamilyProperties(physicalDevices[0], &amountOfQueueFamilyProperties, queueFamilyProperties);
+
+        float *queuePriorities = new float[4] {1.0, 1.0, 1.0, 1.0};
 
         VkDeviceQueueCreateInfo deviceQueueCreateInfo;
         deviceQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
@@ -65,7 +76,7 @@ namespace VulkanPrototype
         deviceQueueCreateInfo.flags = 0;
         deviceQueueCreateInfo.queueFamilyIndex = 0; //Todo Check for Queue Families
         deviceQueueCreateInfo.queueCount = 4;
-        deviceQueueCreateInfo.pQueuePriorities = nullptr;
+        deviceQueueCreateInfo.pQueuePriorities = queuePriorities;
 
         VkPhysicalDeviceFeatures physicalDeviceFeatures = {};
 
@@ -85,7 +96,9 @@ namespace VulkanPrototype
         result = vkCreateDevice(physicalDevices[0], &deviceCreateInfo, nullptr, &device);
         EvaluteVulkanResult(result);
 
+        delete queuePriorities;
         delete physicalDevices;
+        delete layers;
 
         return 0;
     }
