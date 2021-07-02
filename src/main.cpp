37bfbf89dae33ad7b3d3f79acf8c611f8938e914
SiTek <simon@siki>
2021-07-02 20:01:09 +0200

    Added Devices and Layers, removed printDeviceStats function

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@ namespace VulkanPrototype
 {
 
     VkInstance instance;
+    VkDevice device;
 
     void EvaluteVulkanResult(VkResult result)
     {
@@ -19,45 +20,6 @@ namespace VulkanPrototype
         }
     }
 
-    void printPhysicalDeviceStats(VkPhysicalDevice *device)
-    {
-        VkPhysicalDeviceProperties properties;
-        vkGetPhysicalDeviceProperties(*device, &properties);
-        std::cout << "API Version: " << VK_VERSION_MAJOR(properties.apiVersion) << "." << VK_VERSION_MINOR(properties.apiVersion) << "." << VK_VERSION_PATCH(properties.apiVersion) << std::endl;
-        std::cout << "Driver Version: " << VK_VERSION_MAJOR(properties.driverVersion) << "." << VK_VERSION_MINOR(properties.driverVersion) << "." << VK_VERSION_PATCH(properties.driverVersion) << std::endl;
-        std::cout << "Vendor ID: " << properties.vendorID << std::endl;
-        std::cout << "Device ID: " << properties.deviceID << std::endl;
-        std::cout << "Device Type: " << properties.deviceType << std::endl;
-        std::cout << "Device Name: " << properties.deviceName << std::endl;
-
-        VkPhysicalDeviceFeatures features;
-        vkGetPhysicalDeviceFeatures(*device, &features);
-        std::cout << "\nGeometry Shader: " << features.geometryShader << std::endl;
-
-        VkPhysicalDeviceMemoryProperties memoryProperties;
-        vkGetPhysicalDeviceMemoryProperties(*device, &memoryProperties);
-        std::cout << "\nMemory Type Count: " << memoryProperties.memoryTypeCount << std::endl;
-        std::cout << "Memory Heap Count: " << memoryProperties.memoryHeapCount << std::endl;
-
-        uint32_t amountOfQueueFamilies = 0;
-        vkGetPhysicalDeviceQueueFamilyProperties(*device, &amountOfQueueFamilies, nullptr);
-        VkQueueFamilyProperties *familyProperties = new VkQueueFamilyProperties[amountOfQueueFamilies];
-        vkGetPhysicalDeviceQueueFamilyProperties(*device, &amountOfQueueFamilies, familyProperties);
-
-        std::cout << "\nAmount of Queue Families: " << amountOfQueueFamilies << std::endl << std::endl;
-
-        for (int i = 0; i < amountOfQueueFamilies; i++)
-        {
-            std::cout << "Queue Family Number: " << i << std::endl;
-            std::cout << "Queue Graphics Bit: " << (familyProperties[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) << std::endl;
-            std::cout << "Queue Compute Bit: " << (familyProperties[i].queueFlags & VK_QUEUE_COMPUTE_BIT) << std::endl;
-            std::cout << "Queue Transpher Bit: " << (familyProperties[i].queueFlags & VK_QUEUE_TRANSFER_BIT) << std::endl;
-            std::cout << std::endl;
-        }
-
-        delete familyProperties;
-    }
-
     int main(int argc, char *argv[])
     {
         VkApplicationInfo applicationInfo;
@@ -68,6 +30,11 @@ namespace VulkanPrototype
         applicationInfo.pEngineName = nullptr;
         applicationInfo.engineVersion = 0;
         applicationInfo.apiVersion = VK_API_VERSION_1_2;
+
+        uint32_t amountOfLayers = 0;
+        vkEnumerateInstanceLayerProperties(&amountOfLayers, nullptr);
+        VkLayerProperties *layers = new VkLayerProperties[amountOfLayers];
+        vkEnumerateInstanceLayerProperties(&amountOfLayers, layers);
 
         VkInstanceCreateInfo instanceCreateInfo;
         instanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -90,11 +57,31 @@ namespace VulkanPrototype
         result = vkEnumeratePhysicalDevices(instance, &amountOfPhysicalDevices, physicalDevices);
         EvaluteVulkanResult(result);
 
-        for (int i = 0; i < amountOfPhysicalDevices; i++)
-        {
-            printPhysicalDeviceStats(physicalDevices);
-            std::cout << "\n---------------------------------------\n\n";
-        }
+        VkDeviceQueueCreateInfo deviceQueueCreateInfo;
+        deviceQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        deviceQueueCreateInfo.pNext = nullptr;
+        deviceQueueCreateInfo.flags = 0;
+        deviceQueueCreateInfo.queueFamilyIndex = 0; //Todo Check for Queue Families
+        deviceQueueCreateInfo.queueCount = 4;
+        deviceQueueCreateInfo.pQueuePriorities = nullptr;
+
+        VkPhysicalDeviceFeatures physicalDeviceFeatures = {};
+
+        VkDeviceCreateInfo deviceCreateInfo;
+        deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        deviceCreateInfo.pNext = nullptr;
+        deviceCreateInfo.flags = 0;
+        deviceCreateInfo.queueCreateInfoCount = 1;
+        deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo;
+        deviceCreateInfo.enabledLayerCount = 0;
+        deviceCreateInfo.ppEnabledLayerNames = nullptr;
+        deviceCreateInfo.enabledExtensionCount = 0;
+        deviceCreateInfo.ppEnabledExtensionNames = nullptr;
+        deviceCreateInfo.pEnabledFeatures = &physicalDeviceFeatures;
+
+        //Pick fitting Device
+        result = vkCreateDevice(physicalDevices[0], &deviceCreateInfo, nullptr, &device);
+        EvaluteVulkanResult(result);
 
         delete physicalDevices;
 
